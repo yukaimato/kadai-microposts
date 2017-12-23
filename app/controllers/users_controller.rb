57536e1ce3458fcce0de2dc0a3d78bccad435b3f
Yuka Maeno <yuka_imato@yahoo.co.jp>
2017-12-23 13:02:12 +0000

    CRUD

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # new と create はセット
+  # edit と update もセット
+  # new と edit は何か？ → そのための「フォーム」を表示する
+  # create と update は何か？ → 実際に登録 or 更新を実行する
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  # 入力された email を小文字に強制的に変換をする
+  # self.email = 「入力された email」
+  # .downcase! = 「小文字に強制的に変換をする」
+  # なので self.email.upcase! → 大文字
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
 end
